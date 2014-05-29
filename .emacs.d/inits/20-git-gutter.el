94c62c9c064d977d71e7d100ce8aa3bfcb67bdc3
Teppei Kobayashi <teppei.k@me.com>
2014-05-29 20:58:54 +0900

    add git-gutter and configurations.

---
+++ .emacs.d/inits/20-git-gutter.el
@@ -0,0 +1,13 @@
+;
+; 20-git-gutter.el
+; Emacs configuration file
+; Version 1.0
+;
+; Created by Teppei Kobayashi
+; Last Modified 2014/05/28
+;
+
+
+(require 'git-gutter)
+
+(global-git-gutter-mode t)
---
+++ .emacs.d/inits/99-test.el
@@ -0,0 +1,9 @@
+;
+; 99-test.el
+; Emacs configuration file
+; Version - 
+;
+; Created by Teppei Kobayashi
+; Last Modified 2014/05/28
+;
+
