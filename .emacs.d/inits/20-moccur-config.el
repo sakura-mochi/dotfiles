72fd629aa2def7d73e05ce7fbe97b5bce3daf886
Teppei Kobayashi <teppei.k@me.com>
2014-07-26 22:48:02 +0900

    add migemo configuration for Emacs

--- .emacs.d/inits/20-moccur-config.el
+++ .emacs.d/inits/20-moccur-config.el
@@ -1,14 +1,17 @@
 ;
 ; 20-moccur-config.el
 ; Emacs configuration file
-; Version 1.2
+; Version 1.3
 ;
 ; Created by Teppei Kobayashi
-; Last Modified 2013/12/26
-;                                        
+; Last Modified 2014/07/26
+;
 
 (require 'color-moccur)
 
 (load "moccur-edit")
-
 (setq moccur-split-word t)
+
+; when emacs find command "migemo", moccor can use migemo
+(when (executable-find "cmigemo")
+  (setq moccur-use-migemo t))
