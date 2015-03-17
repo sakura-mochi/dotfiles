3746e0ec97f0144ceca587f9d51bf8756e0cec41
Teppei Kobayashi (Chamomile) <teppei.k.el@gmail.com>
2015-03-17 17:31:53 +0900

    some issue fixed.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #
 # install.sh
 # shell script
-# Version 2.1
+# Version 2.2
 #
 # Created by Teppei Kobayashi
-# Last Modified 2015/03/16
+# Last Modified 2015/03/17
 #
 
 
@@ -141,75 +141,15 @@ mkdir dotfiles
 cd dotfiles
 git init
 git remote add dotfiles http://github.com/toronty/dotfiles.git
+git pull dotfiles master
 cd ~
 ln -s dotfiles/.emacs.d/ .emacs.d
 ln -s dotfiles/.zsh/.zshenv .zshenv
 ln -s dotfiles/.screenrc .screenrc
 ln -s dotfiles/.dir_colors .dir_colors
 ln -s dotfiles/.percol.d/ .percol.d
-ln -s dotfiles/.Xresources .Xresorces
-xrdb ~/.Xresorces
-
-
-#
-# OpenCV
-#
-
-sudo apt-get -yV install build-essential
-sudo apt-get -yV install libboost1.46-all-dev
-
-cd /tmp
-sudo apt-get source opencv
-sudo apt-get -yV build-dep opencv
-sudo apt-get -yV install libqt4-dev
-sudo apt-get -yV install libgtk2.0-dev
-sudo apt-get -yV install pkg-config
-sudo apt-get -yV install opencl-headers
-sudo apt-get -yV install libgomp1
-sudo apt-get -yV install libjpeg-dev
-sudo apt-get -yV install libopenjpeg-dev
-sudo apt-get -yV install jasper
-sudo apt-get -yV install libjasper-dev libjasper-runtime
-sudo apt-get -yV install libpng12-dev
-sudo apt-get -yV install libpng++-dev libpng3
-sudo apt-get -yV install libpnglite-dev libpngwriter0-dev libpngwriter0c2
-sudo apt-get -yV install libtiff-dev libtiff-tools pngtools
-sudo apt-get -yV install zlib1g-dev zlib1g-dbg
-sudo apt-get -yV install v4l2ucp
-sudo apt-get -yV install python2.7
-sudo apt-get -yV install python2.7-dev
-sudo apt-get -yV install autoconf
-sudo apt-get -yV install libtbb2 libtbb-dev
-sudo apt-get -yV install libeigen2-dev
-sudo apt-get -yV install cmake
-sudo apt-get -yV install openexr
-sudo apt-get -yV install gstreamer-plugins-*
-sudo apt-get -yV install freeglut3-dev
-sudo apt-get -yV install libglui-dev
-sudo apt-get -yV install libavc1394-dev libdc1394-22-dev libdc1394-utils
-sudo apt-get -yV install libxine-dev
-sudo apt-get -yV install libxvidcore-dev
-sudo apt-get -yV install libva-dev
-sudo apt-get -yV install libssl-dev
-sudo apt-get -yV install libv4l-dev
-sudo apt-get -yV install libvo-aacenc-dev
-sudo apt-get -yV install libvo-amrwbenc-dev
-sudo apt-get -yV install libvorbis-dev
-sudo apt-get -yV install libvpx-dev
-sudo apt-get -yV install libopenni2-dev
-
-cd /tmp
-if [ ! -f OpenCV-2.4.10.zip ]; then
-    wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.10/opencv-2.4.10.zip
-fi
-
-sudo rm -rf opencv-2.4.10
-unzip /tmp/opencv-2.4.10.zip
-cd opencv-2.4.10
-cmake -DBUILD_DOCS=ON -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=RELEASE -DWITH_TBB=ON -DWITH_GTK=ON -DWITH_OPENGL=ON -DWITH_QT=ON -DINSTALL_C_EXAMPLES=ON -DWITH_OPENCL=OFF -DWITH_CUDA=OFF -DWITH_OPENNI=ON -DWITH_UNICAP=ON -DWITH_V4L=ON -DWITH_XINE=ON  .
-make
-sudo make install
-sudo ldconfig
+ln -s dotfiles/.Xresources .Xresources
+xrdb ~/.Xresources
 
 
 # exit
