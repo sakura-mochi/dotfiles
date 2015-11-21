3cfcb895624f0bdc1aab447d1d34ff3659dc6329
Teppei Kobayashi <teppei.k.el@gmail.com>
2015-11-21 19:54:13 +0900

    add minimap for emacs.

---
+++ .emacs.d/inits/20-minimap.el
@@ -0,0 +1,14 @@
+;
+; 20-minimap.el
+; Emacs configuration file
+; Version 1.0
+;
+; Created by Teppei Kobayashi
+; Last Modified 2015/11/21
+;
+
+
+;; minimap.el configuration
+(require 'minimap)
+(setq minimap-window-location 'right)
+(minimap-create)
