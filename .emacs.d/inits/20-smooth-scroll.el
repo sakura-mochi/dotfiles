903d8781561ea61bfede487a43bc891202ee1f42
Teppei Kobayashi <teppei.k.el@gmail.com>
2016-01-15 16:36:59 +0900

    add smooth-scroll

---
+++ .emacs.d/inits/20-smooth-scroll.el
@@ -0,0 +1,14 @@
+;
+; 20-smooth-scroll.el
+; Emacs configuration file
+; Version 1.0
+;
+; Created by Teppei Kobayashi
+; Last Modified 2016/01/15
+;
+
+
+;; smooth-scroll.el configuration
+(require 'smooth-scroll)
+;; auto start smoo-th-scroll
+(smooth-scroll-mode t)
